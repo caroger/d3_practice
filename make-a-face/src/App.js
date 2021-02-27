42f8b5c5557863dee397aeefc9839e02f5e71511
caroger <rogerhu1989@gmail.com>
2021-02-27 15:51:40 -0800

    smiley face

--- make-a-face/src/App.js
+++ make-a-face/src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { arc } from "d3";
+
 const width = 960;
 const height = 500;
 const centerX = width / 2;
@@ -6,20 +9,35 @@ const strokeWidth = 20;
 const eyeOffsetX = 90;
 const eyeOffsetY = 100;
 const eyeRadius = 40;
+const mouthWidth = 20;
+const mouthRadius = 120;
 
-const App = () => (
-  <svg width={width} height={height}>
-    <circle
-      cx={centerX}
-      cy={centerY}
-      r={centerY - strokeWidth / 2}
-      fill="yellow"
-      stroke="black"
-      stroke-width={strokeWidth}
-    />
-    <circle cx={centerX - eyeOffsetX} cy={centerY - eyeOffsetY} r={eyeRadius} />
-    <circle cx={centerX + eyeOffsetX} cy={centerY - eyeOffsetY} r={eyeRadius} />
-  </svg>
-);
+const mouseArc = arc()
+  .innerRadius(mouthRadius)
+  .outerRadius(mouthRadius + mouthWidth)
+  .startAngle(Math.PI / 2)
+  .endAngle((Math.PI * 3) / 2);
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <svg width={width} height={height}>
+          <g transform={`translate(${centerX}, ${centerY})`}>
+            <circle
+              r={centerY - strokeWidth / 2}
+              fill="yellow"
+              stroke="black"
+              stroke-width={strokeWidth}
+            />
+            <circle cx={-eyeOffsetX} cy={-eyeOffsetY} r={eyeRadius} />
+            <circle cx={eyeOffsetX} cy={-eyeOffsetY} r={eyeRadius} />
+            <path d={mouseArc()} />
+          </g>
+        </svg>
+      </div>
+    );
+  }
+}
 
 export default App;
